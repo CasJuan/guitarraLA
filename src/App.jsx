c771785aa6332c44a862fe9245ea7aa196ef09f1
JuanCas <castrojuanig25@gmail.com>
2024-04-07 19:56:43 -0300

    [add] localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,21 @@ function App() {
         }
     }, [auth]) */
 
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart');
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
     const [data,setData] = useState(db);
 
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(initialCart);
 
     const MAX_ITEMS = 5;
     const MIN_ITEMS = 1;
+
+    useEffect (()=>{
+        localStorage.setItem('cart', JSON.stringify(cart))
+    },[cart])
 
     function addToCart (item) {
 
@@ -73,8 +82,6 @@ function App() {
         setCart([]);
     }
     
-
-
 
     /* 
     PARA CONSUMO DE API
