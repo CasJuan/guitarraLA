03436a899fe0d6af05503235e90e85cdae8b9894
JuanCas <castrojuanig25@gmail.com>
2024-04-07 19:22:10 -0300

    [add] utilizando useMemo para mejora de la performance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 //import { Fragment } from "react";
+import { useMemo } from "react";
 
 export default function Header({cart}) {
 
     //State Derivado
-    const isEmpty = () => cart.length === 0;
+    const isEmpty = useMemo( () => cart.length === 0, [cart])
 
-    const cartTotal = () =>  cart.reduce ((total, item) => total + (item.quantity * item.price),0)
+    const cartTotal = useMemo ( () =>  cart.reduce ((total, item) => total + (item.quantity * item.price),0),[cart] )
 
 
     return (
@@ -26,7 +27,7 @@ export default function Header({cart}) {
 
                                 <div id="carrito" className="bg-white p-3">
                                     
-                                    {isEmpty() ? (
+                                    {isEmpty ? (
                                         <p className="text-center">El carrito esta vacio</p>
 
                                     ) : (
@@ -80,7 +81,7 @@ export default function Header({cart}) {
                                             </tbody>
                                         </table>
 
-                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                     </>
                                     )}
 
